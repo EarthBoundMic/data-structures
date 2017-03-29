33fc0dc66be4a75f5493cd90900f6a3d032cf32d
Phong Lam <phongtlam@Stephys-MacBook-Pro.local>
2017-03-29 14:29:48 -0700

    first commit

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,25 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var count = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[count] = value;
+    someInstance[count] = value;
+    count++;
+    //someInstance[count] = value;
   };
 
   someInstance.pop = function() {
+    if (count>0) {
+      count--;
+    }
+    return someInstance[count];
   };
 
   someInstance.size = function() {
+    return count;
   };
 
   return someInstance;
