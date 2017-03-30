25c5adefa80e8cf76cacfd124e341a746888d790
Hack Reactor Student <student@hackreactor.com>
2017-03-29 18:26:36 -0700

    First Commit

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,25 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var storage = {}
+  var someInstance = {
+  	count: 0
+  };
+  _.extend(someInstance ,stackMethods);
+  return someInstance;
 };
 
-var stackMethods = {};
-
+var stackMethods = {
+	push : function (val) {
+		this.count++;
+	},
+	pop : function () {
+		if (this.count > 0) {
+			this.count--;
+		}
+	},
+	size : function () {
+		return this.count;
+	}
+};
 
