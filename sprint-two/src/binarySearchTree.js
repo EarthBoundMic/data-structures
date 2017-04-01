523c87fc8fc8ab64d287d6e509ddb62fe724aea4
Hack Reactor Student <student@hackreactor.com>
2017-04-01 16:56:31 -0700

    Finished hash table and binary tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,51 @@
 var BinarySearchTree = function(value) {
+	var bTree = Object.create(binaryTreeMethods);
+  bTree.value = value;
+  bTree.left = null;
+  bTree.right = null;
+  return bTree;
+};
+
+var binaryTreeMethods = {};
+
+binaryTreeMethods.insert = function(val) {
+  if(val < this.value) {
+    if(this.left === null) {
+      this.left = BinarySearchTree(val);
+    } else {
+      this.left.insert(val);
+    }
+  } else if (val > this.value) {
+    if(this.right === null) {
+      this.right = BinarySearchTree(val);
+    } else {
+      this.right.insert(val);
+    }
+  }
+};
+
+binaryTreeMethods.contains = function(val) {
+  if(val === this.value) {
+    return true;
+  } else if(val < this.value) {
+    if(this.left) {
+      return !!this.left.contains(val);
+    }
+  } else if(val > this.value) {
+    if(this.right) {
+      return !!this.right.contains(val);
+    }
+  }
+};
+
+binaryTreeMethods.depthFirstLog = function(cb) {
+  cb(this.value);
+  if(this.left) {
+    this.left.depthFirstLog(cb);
+  }
+  if(this.right) {
+    this.right.depthFirstLog(cb);
+  }
 };
 
 
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -6,18 +6,44 @@ var HashTable = function() {
 };
 
 HashTable.prototype.insert = function(k, v) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._tupleSearch(k, function(bucket, tuple, i) {
+    var holder = tuple[i];
+    tuple[1] = v;
+    return holder;
+  }, function(bucket) {
+    bucket.push([k, v]);
+  });
 };
 
 HashTable.prototype.retrieve = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._tupleSearch(k, function(bucket, tuple, i) {
+    return tuple[1];
+  });
 };
 
 HashTable.prototype.remove = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._tupleSearch(k, function(bucket, tuple, i) {
+    bucket.splice(i, 1);
+    return tuple[1];
+  });
 };
 
+//below if you want to do high order function...Mic
 
+HashTable.prototype._tupleSearch = function(key, cb, ncb) {
+  var index = getIndexBelowMaxForKey(key, this._limit);
+  var bucket = this._storage.get(index) || [];
+  this._storage.set(index, bucket);
+  for(var i = 0; i < bucket.length; i++) {
+    var tuple = bucket[i];
+    if(tuple[0] === key) {
+      return cb.call(this, bucket, tuple, i);
+    }
+  }
+  if(ncb) {
+    return ncb.call(this, bucket);
+  }
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
